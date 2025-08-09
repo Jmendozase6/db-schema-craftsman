ed70471986bd975b19ec53b80728b02bb8e03510
Jmendozase6 <jmendozase6@ucvvirtual.edu.pe>
2025-08-08 21:32:38 -0500

    fix: comment out Workbench component in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Workbench } from './ui/pages/Workbench.tsx'
+// import { Workbench } from './ui/pages/Workbench.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Workbench />
+    {/* <Workbench /> */}
   </StrictMode>,
 )
